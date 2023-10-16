8044cc8ce50bea2668505a4c030f8523bb2ffd3b
Oscar <oscarcr189@outlook.com>
2023-10-15 18:17:39 -0600

    Tienda Libros-1 Creacion del proyecto

---
+++ src/main/java/cervantes/tiendaLibros/TiendaLibrosApplication.java
@@ -0,0 +1,13 @@
+package cervantes.tiendaLibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaLibrosApplication.class, args);
+	}
+
+}
