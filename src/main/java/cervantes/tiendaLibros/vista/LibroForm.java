938ca6867ce3be7296b1be93ba5db3dc2536eee0
Oscar <oscarcr189@outlook.com>
2023-10-22 20:24:16 -0600

    Tienda Libros-9 agregar libro

--- src/main/java/cervantes/tiendaLibros/vista/LibroForm.java
+++ src/main/java/cervantes/tiendaLibros/vista/LibroForm.java
@@ -1,5 +1,6 @@
 package cervantes.tiendaLibros.vista;
 
+import cervantes.tiendaLibros.modelo.Libro;
 import cervantes.tiendaLibros.servicio.LibroServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -31,9 +32,7 @@ public class LibroForm  extends JFrame {
         this.libroServicio = libroServicio;
         iniciarForma();
 
-        agregarButton.addActionListener(e -> {
-
-        });
+        agregarButton.addActionListener(e -> agregarLibro());
     }
 
     private void iniciarForma(){
@@ -49,6 +48,43 @@ public class LibroForm  extends JFrame {
 
 
     }
+
+    private void agregarLibro(){
+        //Leer valores del formulario
+        if (LibroTexto.getText().equals("")){
+            mostrarMensaje("Proporciona el nombre del libro ");
+            LibroTexto.requestFocusInWindow();
+            return;
+        }
+        var nombreLibro = LibroTexto.getText();
+        var autor = textField1.getText();
+        var precio = Double.parseDouble(precioTexto.getText());
+        var existencia = Integer.parseInt(ExistenciasTexto.getText());
+        //crear el objeto Libro
+        var libro = new Libro(null,nombreLibro,autor,precio,existencia);
+        //libro.setNombrelibro(nombreLibro);
+        //libro.setAutor(autor);
+        //libro.setPrecio(precio);
+        //libro.setExistencias(existencia);
+        this.libroServicio.guardarLibro(libro);
+        mostrarMensaje("Se agrego el libro...");
+        limpiarFormulario();
+        listarLibros();
+
+    }
+
+    private void limpiarFormulario(){
+        LibroTexto.setText("");
+        textField1.setText("");
+        precioTexto.setText("");
+        ExistenciasTexto.setText("");
+
+    }
+
+    private void mostrarMensaje(String mensaje){
+        JOptionPane.showMessageDialog(this, mensaje);
+    }
+
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
